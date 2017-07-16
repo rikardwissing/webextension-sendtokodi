516e6e43234472d36bb91eb8c47ec6e215e7331b
rikardwissing <rikard@nocweb.se>
2017-07-17 00:59:20 +0200

    Added more controls

--- youtube.js
+++ youtube.js
@@ -171,6 +171,44 @@ function onClickHandler(info, tab) {
   if(info.menuItemId == "clearplaylist") ClearPlayList();
   if(info.menuItemId == "playplaylist") PlayPlayList();
   if(info.menuItemId == "hostname") OpenPopup();
+  if(info.menuItemId == "remote_play_pause") RemotePlayPause();
+  if(info.menuItemId == "remote_stop") RemoteStop();
+  if(info.menuItemId == "remote_volume_100") SetVolume(100);
+  if(info.menuItemId == "remote_volume_75") SetVolume(75);
+  if(info.menuItemId == "remote_volume_50") SetVolume(50);
+  if(info.menuItemId == "remote_volume_25") SetVolume(25);
+  if(info.menuItemId == "remote_volume_0") SetVolume(0);
+  if(info.menuItemId == "remote_up") SendCommand('Input.Up');
+  if(info.menuItemId == "remote_down")  SendCommand('Input.Down');
+  if(info.menuItemId == "remote_left") SendCommand('Input.Left');
+  if(info.menuItemId == "remote_right") SendCommand('Input.Right');
+  if(info.menuItemId == "remote_select") SendCommand('Input.Select');
+  if(info.menuItemId == "remote_back") SendCommand('Input.Back');
+  if(info.menuItemId == "remote_home") SendCommand('Input.Home');
+  if(info.menuItemId == "remote_info") SendCommand('Input.Info');
+  if(info.menuItemId == "remote_showosd") SendCommand('Input.ShowOSD');
+  if(info.menuItemId == "remote_rotate") SendCommand('Player.Rotate', '{ "playerid": 1 }');
+  if(info.menuItemId == "remote_hibernate") SendCommand('System.Hibernate');
+  if(info.menuItemId == "remote_reboot") SendCommand('System.Reboot');
+  if(info.menuItemId == "remote_shutdown") SendCommand('System.Shutdown');
+  if(info.menuItemId == "remote_suspend") SendCommand('System.Suspend');
+}
+
+function SendCommand(command, params) {
+  params = params || '[]';
+  SubmitForm('[{"jsonrpc": "2.0", "method": "'+command+'", "params": '+params+', "id": 1}]');
+}
+
+function RemotePlayPause() {
+  SubmitForm('[{"jsonrpc": "2.0", "method": "Player.PlayPause", "params": { "playerid": 1 }, "id": 1}]');
+}
+
+function RemoteStop() {
+  SubmitForm('[{"jsonrpc": "2.0", "method": "Player.Stop", "params": { "playerid": 1 }, "id": 1}]');
+}
+
+function SetVolume(volume) {
+  SubmitForm('[{"jsonrpc": "2.0", "method": "Application.SetVolume", "params": ['+volume+'], "id": 1}]');
 }
 
 function OpenPopup() {
@@ -228,7 +266,14 @@ function createContextMenus() {
   var id = chrome.contextMenus.create({
     type:'separator',
     "contexts": linkContext,
-    "id": "seperator",
+    "id": "playlist_seperator",
+    "parentId": parent
+  });
+
+  var playlist = chrome.contextMenus.create({
+    "title": "Playlist Control",
+    "contexts": allContexts,
+    "id": "playlist",
     "parentId": parent
   });
 
@@ -237,7 +282,7 @@ function createContextMenus() {
     "title": title,
     "contexts": allContexts,
     "id": "clearplaylist",
-    "parentId": parent
+    "parentId": playlist
   });
 
   var title = "Play Playlist";
@@ -245,6 +290,221 @@ function createContextMenus() {
     "title": title,
     "contexts": allContexts,
     "id": "playplaylist",
+    "parentId": playlist
+  });
+
+
+  /* Remote */
+  var id = chrome.contextMenus.create({
+    type:'separator',
+    "contexts": allContexts,
+    "id": "remote_seperator",
     "parentId": parent
+  });
+
+  var remote = chrome.contextMenus.create({
+    "title": "Remote Control",
+    "contexts": allContexts,
+    "id": "remote",
+    "parentId": parent
+  });
+
+  /* PlayPause Stop */
+  var id = chrome.contextMenus.create({
+    "title": "Play / Pause",
+    "contexts": allContexts,
+    "id": "remote_play_pause",
+    "parentId": remote
+  });
+
+  var id = chrome.contextMenus.create({
+    "title": "Stop",
+    "contexts": allContexts,
+    "id": "remote_stop",
+    "parentId": remote
+  });
+
+  /* Volume */
+  var id = chrome.contextMenus.create({
+    type:'separator',
+    "contexts": allContexts,
+    "id": "volume_seperator",
+    "parentId": remote
+  });
+
+  var volume = chrome.contextMenus.create({
+    "title": "Volume",
+    "contexts": allContexts,
+    "id": "remote_volume",
+    "parentId": remote
+  });
+
+  var id = chrome.contextMenus.create({
+    "title": "Volume 100%",
+    "contexts": allContexts,
+    "id": "remote_volume_100",
+    "parentId": volume
+  });
+
+  var id = chrome.contextMenus.create({
+    "title": "Volume 75%",
+    "contexts": allContexts,
+    "id": "remote_volume_75",
+    "parentId": volume
+  });
+
+  var id = chrome.contextMenus.create({
+    "title": "Volume 50%",
+    "contexts": allContexts,
+    "id": "remote_volume_50",
+    "parentId": volume
+  });
+
+  var id = chrome.contextMenus.create({
+    "title": "Volume 25%",
+    "contexts": allContexts,
+    "id": "remote_volume_25",
+    "parentId": volume
+  });
+
+  var id = chrome.contextMenus.create({
+    "title": "Volume 0%",
+    "contexts": allContexts,
+    "id": "remote_volume_0",
+    "parentId": volume
+  });
+
+  /* */
+  var id = chrome.contextMenus.create({
+    type:'separator',
+    "contexts": allContexts,
+    "id": "more_seperator",
+    "parentId": remote
+  });
+
+  var more = chrome.contextMenus.create({
+    "title": "Input",
+    "contexts": allContexts,
+    "id": "remote_more",
+    "parentId": remote
+  });
+
+  var id = chrome.contextMenus.create({
+    "title": "Select",
+    "contexts": allContexts,
+    "id": "remote_select",
+    "parentId": more
+  });
+
+  var id = chrome.contextMenus.create({
+    "title": "Back",
+    "contexts": allContexts,
+    "id": "remote_back",
+    "parentId": more
+  });
+
+  var id = chrome.contextMenus.create({
+    type:'separator',
+    "contexts": allContexts,
+    "id": "directional_seperator",
+    "parentId": more
+  });
+
+  var id = chrome.contextMenus.create({
+    "title": "Up",
+    "contexts": allContexts,
+    "id": "remote_up",
+    "parentId": more
+  });
+
+  var id = chrome.contextMenus.create({
+    "title": "Left",
+    "contexts": allContexts,
+    "id": "remote_left",
+    "parentId": more
+  });
+
+  var id = chrome.contextMenus.create({
+    "title": "Right",
+    "contexts": allContexts,
+    "id": "remote_right",
+    "parentId": more
+  });
+
+  var id = chrome.contextMenus.create({
+    "title": "Down",
+    "contexts": allContexts,
+    "id": "remote_down",
+    "parentId": more
+  });
+
+  var id = chrome.contextMenus.create({
+    type:'separator',
+    "contexts": allContexts,
+    "id": "other_seperator",
+    "parentId": more
+  });
+
+  var id = chrome.contextMenus.create({
+    "title": "Info",
+    "contexts": allContexts,
+    "id": "remote_info",
+    "parentId": more
+  });
+
+  var id = chrome.contextMenus.create({
+    "title": "Show OSD",
+    "contexts": allContexts,
+    "id": "remote_showosd",
+    "parentId": more
+  });
+
+  var id = chrome.contextMenus.create({
+    "title": "Home",
+    "contexts": allContexts,
+    "id": "remote_home",
+    "parentId": more
+  });
+
+  var id = chrome.contextMenus.create({
+    type:'separator',
+    "contexts": allContexts,
+    "id": "system_seperator",
+    "parentId": remote
+  });
+
+  var system = chrome.contextMenus.create({
+    "title": "System",
+    "contexts": allContexts,
+    "id": "system",
+    "parentId": remote
+  });
+
+  var id = chrome.contextMenus.create({
+    "title": "Hibernate",
+    "contexts": allContexts,
+    "id": "remote_hibernate",
+    "parentId": system
+  });
+
+  var id = chrome.contextMenus.create({
+    "title": "Reboot",
+    "contexts": allContexts,
+    "id": "remote_reboot",
+    "parentId": system
+  });
+
+  var id = chrome.contextMenus.create({
+    "title": "Shutdown",
+    "contexts": allContexts,
+    "id": "remote_shutdown",
+    "parentId": system
+  });
+
+  var id = chrome.contextMenus.create({
+    "title": "Suspend",
+    "contexts": allContexts,
+    "id": "remote_suspend",
+    "parentId": system
   });
 }
